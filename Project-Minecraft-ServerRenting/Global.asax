893d68c5af31d6b95024f9180a157ceba1e78c47
megagamedk <mg@megagame.dk>
2017-02-20 12:31:50 +0100

    Add project files.

---
+++ Project-Minecraft-ServerRenting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_Minecraft_ServerRenting.MvcApplication" Language="C#" %>
---
+++ Project-Minecraft-ServerRenting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Minecraft_ServerRenting.Startup))]
+namespace Project_Minecraft_ServerRenting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
